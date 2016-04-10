46954777bf440f432bdeea87ac9dca3a3dfb53df
nickpandolfi <npandolfi@wpi.edu>
2016-04-10 13:27:12 -0400

    Cleaned up a few bugs and updated changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 
 NAME = 'Cyther'
-VERSION = '0.4.3.dev0'
+VERSION = '0.5.0'
 SHORT_DESCRIPTION = 'The Cross-Platform Cython/Python Compiler'
 LONG_DESCRIPTION = open('README.txt').read()
 PACKAGES = ['cyther']
