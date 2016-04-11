c2e10ecc5ac39d739560fd25f484773104e09071
nickpandolfi <npandolfi@wpi.edu>
2016-04-11 16:34:27 -0400

    Did some reordering of functions

--- cyther/cyther.py
+++ cyther/cyther.py
@@ -6,10 +6,6 @@ import os, sys, subprocess, platform
 import re, collections
 import time, argparse
 
-MSVC_VERSION = int(msvccompiler.get_build_version())
-PLATFORM = sys.platform
-BASENAME = "python" + sysconfig.get_python_version()
-
 
 class CytherError(Exception):
     """A helpful custom error to be called when a general python error just doesn't make sense in context"""
@@ -22,104 +18,13 @@ try:
 except ImportError:
     raise CytherError("The current version of Python doesn't support the function 'which', normally located in shutil")
 
-MAJOR = str(sys.version_info.major)
-MINOR = str(sys.version_info.minor)
 
-VER = MAJOR + MINOR
-
-CYTHER_FAIL = 0
-CYTHER_SUCCESS = 1
-ERROR_PASSOFF = -3
-INTERVAL = .25
-
-CYTHONIZABLE_FILE_EXTS = ('.pyx', '.py') # TODO make a regular expression right here
-
-NOT_NEEDED_MESSAGE = "Module '{}' does not have to be included, or has no .get_include() method"
-
-PYTHON_DIRECTORY = sys.exec_prefix
-DRIVE_AND_NAME = os.path.splitdrive(PYTHON_DIRECTORY)
-PYTHON_NAME = 'python' + MAJOR + '.' + MINOR # os.path.basename(DRIVE_AND_NAME[1]).lower()
-DRIVE = DRIVE_AND_NAME[0]
-if not DRIVE:
-    DRIVE = os.path.normpath('/')
-
-
-EXPRESSIONS = collections.defaultdict() # will always return everything
-EXPRESSIONS['include'] = r'(.+)([pP][yY][tT][hH][oO][nN])(\s?' + MAJOR + r'\.?' + MINOR + \
-                         r'(\.\d)?)(\/|\\)(include)(\/|\\)?(\n|$)'
-EXPRESSIONS['libs'] = r'(.+)([pP][yY][tT][hH][oO][nN])(\s?' + MAJOR + r'\.?' + MINOR + \
-                      r'(\.\d)?)(\/|\\)(libs)(\/|\\)?(\n|$)'
-
-EXPRESSIONS['python'] = None
-
-CYTHER_CONFIG_FILE = os.path.join(os.path.expanduser('~'), '.cyther')
-
-LIB_A_MISSING_MESSAGE = '''
-Currently, cyther does not support the automatic construction of 'libpython{0}.a',
-so you will have to do it yourself. There are several tools that can help you with
-this, including dlltool, gendef, pexport, reimp. All of which are a part of mingw32.
-
-The general idea is to first look into the python{0}.dll and figure out all the
-objects that belong to that file. Then, take the names of these objects, and put
-them in a .def file. Take this file, and pass it to dll tool to tell it what to
-extract from the python{0}.dll and inject into the libpython{0}.a static library.
-'''
-
-ASSUMPTIONS = """
-Assumptions cyther makes about your system:
-1) Cython and gcc are both installed, and accessible from the system console
-2) Python supports 'shutil.which'
-3) Your environment path variable is able to be found by `shutil.which`
-4) gcc can work with the option -l pythonXY (libpythonXY.a exists in your python libs directory)
-5) Almost any gcc compiled C program will work on Windows
-6) Python's 'libs' and 'include' directories are in the same drive that python is installed
-"""
-
-def crawl(*to_find, source=DRIVE):
-    """This function will wrap the shutil.which function to return the abspath every time, or a empty string"""
-    found = {}
-    cmd = set(to_find)
-
-    for root, dirs, _ in os.walk(source):
-        for directory in cmd.intersection(set(dirs)):
-            if directory in cmd:
-                string = os.path.abspath(os.path.join(source, root, directory))
-                expression = EXPRESSIONS[directory]
-                dna = False
-
-                if expression:
-                    matches = re.search(expression, string)
-                    if (not matches) or (matches.end() != len(string)):
-                        dna = True
-
-                if not dna:
-                    if directory not in found:
-                        found[directory] = []
-                    found[directory].append(string)
-
-    for item in cmd:
-        if item not in found:
-            raise CytherError("The item '{}' was not found searching drive '{}'\n\n'{}'".format(item, DRIVE, found[item]))
-        elif len(found[item]) > 1:
-            raise CytherError("The item '{}' was found more than once in drive '{}'\n\n'{}'".format(item, DRIVE, found[item]))
-
-    return {item: found[item][0] for item in found}
-
-
-def where(cmd, mode=os.X_OK, path=None):
-    raw_result = which(cmd, mode, path)
-    if raw_result:
-        return os.path.abspath(raw_result)
-    else:
-        raise CytherError("Could not find '{}' in the path".format(cmd))
+def dealWithLibA(message):
+    """What to do if the libpythonXY (.a | .so) is missing. Currently, it raises an error, and prints a helpful message"""
+    raise CytherError(message)
 
 
 def getDirs():
-    # What is the difference between os.name and sys.platform
-    # Why do some return operating systems, and some return processor architectures
-    # Get current path instead of using '.'
-    # Figure out where 'user' comes from
-
     include_dirs, library_dirs = [], []
 
     py_include = sysconfig.get_python_inc()
@@ -190,10 +95,100 @@ def getDirs():
 
     return ret_object
 
+########################################################################################################################
+########################################################################################################################
 
-def dealWithLibA(direc, message):
-    """What to do if the libpythonXY.a is missing. Currently, it raises an error, and prints a helpful message"""
-    raise CytherError("The file '{}' does not exist.\n\n{}".format(direc, message))
+CYTHER_FAIL = 0
+CYTHER_SUCCESS = 1
+ERROR_PASSOFF = -3
+INTERVAL = .25
+
+MAJOR = str(sys.version_info.major)
+MINOR = str(sys.version_info.minor)
+
+VER = MAJOR + MINOR
+
+MSVC_VERSION = int(msvccompiler.get_build_version())
+PLATFORM = sys.platform
+BASENAME = "python" + sysconfig.get_python_version()
+
+CYTHONIZABLE_FILE_EXTS = ('.pyx', '.py')
+
+PYTHON_DIRECTORY = sys.exec_prefix
+DRIVE_AND_NAME = os.path.splitdrive(PYTHON_DIRECTORY)
+PYTHON_NAME = 'python' + MAJOR + '.' + MINOR
+PYTHON_NAME_2 = os.path.basename(DRIVE_AND_NAME[1]).lower()
+DRIVE = DRIVE_AND_NAME[0]
+
+if not DRIVE:
+    DRIVE = os.path.normpath('/')
+
+CYTHER_CONFIG_FILE = os.path.join(os.path.expanduser('~'), '.cyther')
+
+INCLUDE_DIRS, RUNTIME_DIRS = getDirs()
+
+EXPRESSIONS = collections.defaultdict()
+
+MISSING_INCLUDE_DIRS = """
+Cyther could not find any include directories that the current Python installation was built off of.
+This is eiher a bug or you don't have Python correctly installed.
+"""
+
+MISSING_RUNTIME_DIRS = """
+Cyther could not find any runtime libraries that the current Python installation was built off of.
+This is eiher a bug or you don't have Python correctly installed.
+"""
+
+NOT_NEEDED_MESSAGE = "Module '{}' does not have to be included, or has no .get_include() method"
+
+
+if not INCLUDE_DIRS:
+    raise CytherError(MISSING_INCLUDE_DIRS)
+
+if not RUNTIME_DIRS:
+    dealWithLibA(MISSING_RUNTIME_DIRS)
+
+########################################################################################################################
+########################################################################################################################
+
+
+def crawl(*to_find, source=DRIVE):
+    """This function will wrap the shutil.which function to return the abspath every time, or a empty string"""
+    found = {}
+    cmd = set(to_find)
+
+    for root, dirs, _ in os.walk(source):
+        for directory in cmd.intersection(set(dirs)):
+            if directory in cmd:
+                string = os.path.abspath(os.path.join(source, root, directory))
+                expression = EXPRESSIONS[directory]
+                dna = False
+
+                if expression:
+                    matches = re.search(expression, string)
+                    if (not matches) or (matches.end() != len(string)):
+                        dna = True
+
+                if not dna:
+                    if directory not in found:
+                        found[directory] = []
+                    found[directory].append(string)
+
+    for item in cmd:
+        if item not in found:
+            raise CytherError("The item '{}' was not found searching drive '{}'\n\n'{}'".format(item, DRIVE, found[item]))
+        elif len(found[item]) > 1:
+            raise CytherError("The item '{}' was found more than once in drive '{}'\n\n'{}'".format(item, DRIVE, found[item]))
+
+    return {item: found[item][0] for item in found}
+
+
+def where(cmd, mode=os.X_OK, path=None):
+    raw_result = which(cmd, mode, path)
+    if raw_result:
+        return os.path.abspath(raw_result)
+    else:
+        raise CytherError("Could not find '{}' in the path".format(cmd))
 
 
 def getFullPath(filename):
@@ -217,6 +212,7 @@ def processFiles(args):
     include_directories = ''
     for d in i:
         include_directories += '-I' + d
+
     libs_directories = ''
     for d in l:
         libs_directories += '-L' + d
@@ -225,6 +221,7 @@ def processFiles(args):
         file = dict()
         file['include'] = include_directories
         file['libs'] = libs_directories
+        file['l_option'] = '-l' + sift(getDirs()[1])
         file['file_path'] = getFullPath(filename)
         file['file_base_name'] = os.path.splitext(os.path.basename(file['file_path']))[0]
         file['no_extension'], file['extension'] = os.path.splitext(file['file_path'])
@@ -274,7 +271,7 @@ def isOutDated(file):
     return False
 
 
-def sift(obj): # TODO Is it necessary to make it so that this will return multiple results??
+def sift(obj):
     string = [str(item) for name in obj for item in os.listdir(name)]
     s = set(re.findall('(?<=lib)(.+?)(?=\.so|\.a)', '\n'.join(string)))
     result = max(list(s), key=len)
@@ -283,7 +280,6 @@ def sift(obj): # TODO Is it necessary to make it so that this will return multip
 
 def makeCommands(preset, file):
     """Given a high level preset, it will construct the basic args to pass over. 'ninja', 'beast', and 'minimal'"""
-    PYTHON_NAME = sift(getDirs()[1])
 
     if not preset:
         preset = 'ninja'
@@ -291,22 +287,24 @@ def makeCommands(preset, file):
     if preset == 'ninja':
         cython_command = ['cython', '-a', '-p', '-o', file['c_name'], file['file_path']]
         gcc_command = ['gcc', '-fPIC', '-shared', '-w', '-O3', file['include'], file['libs'], '-o',
-                       file['output_name'], file['c_name'], '-l', PYTHON_NAME]
+                       file['output_name'], file['c_name'], file['l_option']]
     elif preset == 'beast':
         cython_command = ['cython', '-a', '-l', '-p', '-o', file['c_name'], file['file_path']]
         gcc_command = ['gcc', '-fPIC', '-shared', '-Wall', '-O3', file['include'], file['libs'], '-o',
-                       file['output_name'], file['c_name'], '-l', PYTHON_NAME]
+                       file['output_name'], file['c_name'], file['l_option']]
     elif preset == 'minimal':
         cython_command = ['cython', '-o', file['c_name'], file['file_path']]
         gcc_command = ['gcc', '-fPIC', '-shared', file['include'], file['libs'], '-o', file['output_name'],
-                       file['c_name'], '-l', PYTHON_NAME]
+                       file['c_name'], file['l_option']]
+    elif preset == 'swift':
+        pass
     else:
         raise CytherError("The preset '{}' is not supported".format(preset))
     return cython_command, gcc_command
 
 
 def printCommands(commands):
-    """Simply prints all of the commands on screen for the user to see. Nice for debugging."""
+    """Simply prints all of the commands to stdout for the user to see. Nice for debugging."""
     print(' '.join(commands).strip())
 
 
@@ -351,14 +349,8 @@ def getDirsToInclude(string):
     return dirs
 
 
-#def callCompilers(args, commands, actions):
 def callCompilers(args, commands):
-    """
-    The function where the actual compilation takes place
-
-    """
-    # TODO what does actions accomplish in this code??
-    #for command, action in zip(commands, actions):
+    """The function where the actual compilation takes place"""
     for command in commands:
         ret = subprocess.call(command)
         if ret:
@@ -384,14 +376,11 @@ def cytherize(args, file, print_command):
     filterCommands(cython_pass_off, cython_commands)
     filterCommands(gcc_pass_off, gcc_commands)
 
-    #dirs = getDirsToInclude(args['include'])
-    #gcc_commands.extend(dirs)
-
     if print_command:
         printCommands(cython_commands)
         printCommands(gcc_commands)
 
-    response = callCompilers(args, (cython_commands, gcc_commands)) # , ('cython', 'gcc'))
+    response = callCompilers(args, (cython_commands, gcc_commands)) # This is where to compile only C code as well
     return response
 
 
@@ -456,52 +445,32 @@ def core(args):
         else:
             time.sleep(interval)
 
-# Updated the operating system finder to be safer.
-# Fixed the hardcoding of the python environment structure to not be primary.
 
 OPERATING_SYSTEM = platform.platform()
+
 IS_WINDOWS = OPERATING_SYSTEM.lower().startswith('windows')
 
-#LIBRARY_EXTENSION = '.dll' if IS_WINDOWS else '.so'
 DEFAULT_OUTPUT_EXTENSION = '.pyd' if IS_WINDOWS else '.so'
 
 
-STATIC = 'lib' + PYTHON_NAME + '.a'
-#DLL_NAME = PYTHON_NAME + LIBRARY_EXTENSION
-
-where('python')
-where('cython')
-where('gcc')
-
-#DIRECTORY_FINDINGS = crawl('libs', 'include') # To make sure they exist, and find the paths
-
-#LIBS_DIRECTORY = DIRECTORY_FINDINGS['libs']
-#INCLUDE_DIRECTORY = DIRECTORY_FINDINGS['include']
-#DLL_DIRECTORY = DIRECTORY_FINDINGS[DLL_NAME]
-
-
-#LIB_A_DIRECTORY = os.path.normpath(os.path.join(LIBS_DIRECTORY, STATIC))  # Have to hardcode this one in
-#if not os.path.exists(LIB_A_DIRECTORY):
-#    dealWithLibA(LIB_A_DIRECTORY, LIB_A_MISSING_MESSAGE.format(VER))
+PYTHON_EXECUTABLE = where('python')
+CYTHON_EXECUTABLE = where('cython')
+GCC_EXECUTABLE = where('gcc')
 
 INFO = str()
 INFO += "\nSystem:"
 
-INFO += "\n\tPython:"
+INFO += "\n\tPython ({}):".format(PYTHON_EXECUTABLE)
 INFO += "\n\t\tVersion: {}".format('.'.join(list(VER)))
 INFO += "\n\t\tOperating System: {}".format(OPERATING_SYSTEM)
 INFO += "\n\t\t\tOS is Windows: {}".format(IS_WINDOWS)
-#INFO += "\n\t\tLinked Library Extension: {}".format(LIBRARY_EXTENSION)
 INFO += "\n\t\tDefault Output Extension: {}".format(DEFAULT_OUTPUT_EXTENSION)
 INFO += "\n\t\tInstallation Directory: {}".format(PYTHON_DIRECTORY)
-#INFO += "\n\t\tDirectory 'libs' Location: {}".format(LIBS_DIRECTORY)
-#INFO += "\n\t\tDirectory 'include' Location: {}".format(INCLUDE_DIRECTORY)
-#INFO += "\n\t\tLibrary '.a' Location: {}".format(LIB_A_DIRECTORY)
-#INFO += "\n\t\tPython '{}' Location: {}".format(LIBRARY_EXTENSION, DLL_DIRECTORY)
 
-INFO += "\n\tCython:"
+INFO += "\n\tCython ({}):".format(CYTHON_EXECUTABLE)
 INFO += "\n\t\tNothing Here Yet"
-INFO += "\n\tGCC:"
+
+INFO += "\n\tGCC ({}):".format(GCC_EXECUTABLE)
 INFO += "\n\t\tNothing Here Yet"
 
 __cytherinfo__ = INFO
@@ -521,11 +490,12 @@ help_watch = "When given, cyther will watch the directory with the 't' option im
 help_cython = "Arguments to pass to Cython (use '_' or '__' instead of '-' or '--'"
 help_gcc = "Arguments to pass to gcc (use '_' or '__' instead of '-' or '--'"
 
-description_text = 'Auto compile and build .pyx files in place.\n{}\n{}'
+description_text = 'Auto compile and build .pyx files in place.\n{}'
+description = description_text.format(__cytherinfo__)
+formatter = argparse.RawDescriptionHelpFormatter
+usage = 'cyther [options] input_file'
 
-parser = argparse.ArgumentParser(description=description_text.format(ASSUMPTIONS, __cytherinfo__),
-                                 formatter_class=argparse.RawDescriptionHelpFormatter,
-                                 usage='cyther [options] input_file')
+parser = argparse.ArgumentParser(description=description, formatter_class=formatter, usage=usage)
 parser.add_argument('filenames', action='store', nargs='+', help=help_filenames)
 parser.add_argument('-p', '--preset', action='store', default='', help=help_preset)
 parser.add_argument('-t', '--timestamp', action='store_true', default=False, help=help_timestamp)
