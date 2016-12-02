46a483598fe3ad48cec25bce17d48354493ae5a7
nickpandolfi <npandolfi@wpi.edu>
2016-12-01 22:11:34 -0500

    Completed function 'createPath' [ci skip]

--- cyther/files.py
+++ cyther/files.py
@@ -12,26 +12,27 @@ import os
 DRIVE = 0
 EXTENSION = 1
 NAME = 0
+OVERWRITE_ERROR = "; cannot overwrite without explicit permission"
 
 
-def getParentDir(filename):
+def getParentDir(path):
     """
     This will return just the name of the parent directory of
     the given file path
     """
-    return os.path.basename(os.path.dirname(filename))
+    return os.path.basename(os.path.dirname(path))
 
 
-def injectCache(filename):
+def injectCache(path):
     """
     This will inject a '__cythercache__' into a file path before the filename
     and after the parent directory, essentially putting it in the cache
     """
-    if getParentDir(filename) == '__cythercache__':
+    if getParentDir(path) == '__cythercache__':
         raise FileExistsError("Can't put a cache in another cache")
-    new_filename = os.path.join(os.path.dirname(filename),
+    new_filename = os.path.join(os.path.dirname(path),
                                 '__cythercache__',
-                                os.path.basename(filename))
+                                os.path.basename(path))
     return new_filename
 
 
@@ -46,31 +47,31 @@ def joinExt(name, ext):
     return ret
 
 
-def changeFileName(filename, new_name):
+def changeFileName(path, new_name):
     """
     Changes the name of the file with the extension from the given path name
     """
-    dirname = os.path.dirname(filename)
-    old_basename = os.path.basename(filename)
+    dirname = os.path.dirname(path)
+    old_basename = os.path.basename(path)
     new_basename = joinExt(new_name, os.path.splitext(old_basename)[1])
     return os.path.join(dirname, new_basename)
 
 
-def changeFileDir(filename, dirname):
+def changeFileDir(path, dirname):
     """
     Changes the parent directory of the given path name
     """
-    return os.path.join(dirname, os.path.basename(filename))
+    return os.path.join(dirname, os.path.basename(path))
 
 
-def changeFileExt(filename, ext):
+def changeFileExt(path, ext):
     """
     Changes the extension of the file to ext
     """
-    return joinExt(os.path.splitext(filename)[0], ext)
+    return joinExt(os.path.splitext(path)[0], ext)
 
 
-# TODO detect if it is a file or directory fragment
+# TODO Implement a must_exist parameter?
 def detectPath(path, *, isfile=None, isdir=None):
     """
     Detect what the path represents. Must provide a single mutually exclusive
@@ -96,16 +97,11 @@ def detectPath(path, *, isfile=None, isdir=None):
     return result
 
 
-def getFullPath(path=None, *, must_exist=True, error=True, root=None,
-                absolute=False):
+def getFullPath(path=None, *, must_exist=True, error=True):
     """
     Gets the full absolute path of a given path
     """
     if path:
-        if root:
-            if absolute and not os.path.splitdrive(root)[DRIVE]:
-                raise ValueError("Parameter 'root' must be a full path name")
-            path = os.path.join(root, path)
         abspath = os.path.abspath(path)
         if must_exist:
             if os.path.exists(abspath):
@@ -124,29 +120,61 @@ def getFullPath(path=None, *, must_exist=True, error=True, root=None,
     return full_path
 
 
-def getExtension(path):
+###############################################################################
+
+
+def hasExtension(fragment):
     """
-    Fetch the extension of the path name 'path'
+    Returns a boolean value to denote if the path fragment 'fragment'
+    has an extension
     """
-    return os.path.splitext(path)[EXTENSION]
+    return bool(os.path.splitext(fragment)[EXTENSION])
 
 
-def getName(path):
-    pass
+def getExtension(fragment):
+    """
+    Fetch the extension of the partial path 'fragment'
+    """
+    return os.path.splitext(fragment)[EXTENSION]
 
 
-def getFileName(path):
-    pass
+def hasName(fragment):
+    """
+    Return a boolean value to denote if the path fragment 'fragment' has a
+    name (test is done by seeing if the fragment has a extension)
+    """
+    return hasExtension(fragment)
+
+
+def getName(fragment, *, ext=False):
+    """
+    Fetch the file name of the partial path 'fragment'
+    """
+    basename = os.path.basename(fragment)
+    if ext:
+        result = basename
+    else:
+        result = os.path.splitext(basename)[NAME]
+    return result
 
 
 def getDirectory(path):
+    """
+    Gets the directory part of the path provided
+    """
     if detectPath(path, isdir=True):
         directory = path
     else:
         directory = os.path.dirname(path)
     return directory
 
-OVERWRITE_ERROR = "; cannot overwrite without explicit permission"
+
+def isAbsolute(path):
+    """
+    Returns if the given path is an absolute path or not.
+    """
+
+    return os.path.abspath(path) == os.path.normpath(path)
 
 
 # TODO rename error to make more sense (it only blocks one kind of error)
@@ -166,23 +194,23 @@ def createPath(path=None, *, name=None, ext=None, inject=None, root=None,
                 if not overwrite:
                     raise ValueError("The name supplied must not have "
                                      "an extension" + OVERWRITE_ERROR)
-                new_name = joinExt(getJustName(name), ext)
+                new_name = joinExt(getName(name, ext=False), ext)
             else:
                 new_name = name
         else:
             if ext:
                 new_name = joinExt(name, ext)
             else:
-                new_name = getJustName(name)
+                new_name = getName(name, ext=False)
     else:
         if detectPath(path, isfile=True):
             if ext:
                 if not overwrite:
                     raise ValueError("Can't provide an extension with a full "
                                      "file name" + OVERWRITE_ERROR)
-                new_name = joinExt(getJustName(name), ext)
+                new_name = joinExt(getName(name, ext=False), ext)
             else:
-                new_name = name
+                new_name = getName(name, ext=True)
         else:
             raise ValueError("The path specified is a directory, and no name "
                              "was provided; therefore, no name exists "
@@ -209,7 +237,7 @@ def createPath(path=None, *, name=None, ext=None, inject=None, root=None,
     if inject:
         new_directory = os.path.join(new_directory, inject)
 
-    abspath = os.path.join(new_directory, name)
+    abspath = os.path.join(new_directory, new_name)
     if must_exist:
         if not os.path.exists(abspath):
             result = abspath
@@ -223,79 +251,6 @@ def createPath(path=None, *, name=None, ext=None, inject=None, root=None,
     return result
 
 
-"""
-If inject
-    NEW_DIRECTORY = NEW_DIRECTORY + inject
-
-abspath = os.path.join(NEW_DIRECTORY, NEW_NAME)
-if must_exist
-    if abspath exists
-        result = abspath
-    else
-        if exists_error
-            ERROR (The path doesn't exist)
-        else
-            result = None
-else
-    result = abspath
-
-return result
-"""
-
-
-"""
-if name
-    If path and path has a name
-        if not overwrite
-            ERROR (Path must be a directory)
-
-    If name has an extension
-        if ext is given
-            if not overwrite:
-                ERROR (cannot overwrite the name's extension, strip it first)
-            NEW_NAME = name(w/o ext) + given_ext
-        else
-            NEW_NAME = name
-    else
-        if ext is given
-            NEW_NAME = name + given_ext
-        else
-            NEW_NAME = name(w/o ext)
-else
-    if path is a file, extract file name
-        if ext:
-            if not overwrite:
-                ERROR
-            NEW_NAME = name(w/o ext) + given_ext
-        else:
-            NEW_NAME = name
-    else
-        ERROR (path is a directory, no name exists)
-"""
-"""
-If root
-    If root is not absolute
-        ERROR (root must be an absolute directory if specified)
-    if path is absolute
-        ERROR (cannot add two absolute paths together...)
-
-    If path
-        NEW_DIRECTORY = root + dirname(path)
-    else
-        NEW_DIRECTORY = root
-else
-    If path
-        path = getDirectory(path)
-        If path is a directory
-            NEW_DIRECTORY = abspath(path)
-        else
-            NEW_DIRECTORY = dirname(path)
-    else
-        NEW_DIRECTORY = cwd
-"""
-
-
-# TODO Add the getFullPath and convert functions together?
 class File:
     """
     Holds all of the information and methods necessary to process full paths.
@@ -333,51 +288,14 @@ class File:
         """
         Method that works the same as os.path.exists, but on the internal path
         """
-        return self.__file_exists
+        return os.path.exists(self.getPath())
 
-    def create(self, *, directory=None, name=None, extension=None):
+    def createPath(self, **kwargs):
         """
         Returns a different object with the specified changes applied to
         it. This object is not changed in the process.
         """
-        path = self.getPath()
-        if directory:
-            new_directory = directory
-        else:
-            new_directory = os.path.dirname(path)
-
-        if extension:
-            new_extension = extension
-        else:
-            if name:
-                new_extension = os.path.splitext(name)[EXTENSION]
-            else:
-                new_extension = os.path.splitext(path)[EXTENSION]
-
-        if not new_extension:
-            if not name:
-                raise ValueError("You must provide an extension if "
-                                 "one was not specified in the original "
-                                 "'File' definition")
-            else:
-                raise ValueError("A name must include an extension with the "
-                                 "file's base name")
-
-        if new_extension[0] != '.':
-            new_extension = '.' + new_extension
-
-        # Now that we know there is an extension, that means basename == name
-        # Name only exists if there is a file, else nope
-
-        split = os.path.splitext(os.path.basename(path))
-        if split[EXTENSION]:
-            raw_name = split[NAME]
-        else:
-            raise ValueError("You cannot specify a different extension when "
-                             "the original file path was a directory")
-
-        filename = os.path.join(raw_name, new_extension)
-        new_path = os.path.join(new_directory, filename)
+        new_path = createPath(self.getPath(), **kwargs)
         return File(new_path)
 
     def isOutDated(self, output_file):
@@ -385,11 +303,9 @@ class File:
         Figures out if Cyther should compile the given FileInfo object by
         checking the both of the modified times
         """
-        # TODO incorporate the getmtime method in here
-        path = output_file.getPath()
-        if os.path.exists(path):
-            source_time = os.path.getmtime(self.getPath())
-            output_time = os.path.getmtime(path)
+        if output_file.exists():
+            source_time = self.getmtime()
+            output_time = output_file.getmtime()
             if source_time > output_time:
                 return True
         else:
@@ -397,12 +313,11 @@ class File:
         return False
 
     def isUpdated(self, previous_modified_time):
-        # TODO Why didn't I use abspath or normpath?
         """
         Figures out if the file had previously errored and hasn't
         been fixed since given a numerical time
         """
-        modified_time = os.path.getmtime(self.getPath())
+        modified_time = self.getmtime()
         valid = modified_time > previous_modified_time
         return valid
 
@@ -434,5 +349,5 @@ class File:
 
 if __name__ == '__main__':
     file = File()
-    new = file.create(extension='.p')
+    new = file.createPath(extension='.p')
     print(new)
