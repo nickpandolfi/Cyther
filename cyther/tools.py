f1224e0ac83e3684f0c7329e4ae3606925d66b83
nickpandolfi <npandolfi@wpi.edu>
2016-12-20 20:07:41 -0500

    Developing and cleaning

--- cyther/tools.py
+++ cyther/tools.py
@@ -1,11 +1,43 @@
 
-from .definitions import NOT_NEEDED_MESSAGE, RESPONSES_ERROR
+import os
 
 
 class CytherError(Exception):
     """A custom error used to denote that an exception was Cyther related"""
     def __init__(self, *args, **kwargs):
         super(CytherError, self).__init__(*args, **kwargs)
+
+
+def write_dict_to_file(file_path, obj):
+    """
+    Write a dictionary of strings to a file
+    """
+    string = ''
+    for key, value in obj.items():
+        string += key + ':' + value + os.linesep
+
+    with open(file_path, 'w+') as file:
+        chars = file.write(string)
+
+    return chars
+
+
+def read_dict_from_file(file_path):
+    """
+    Read a dictionary of strings from a file
+    """
+    with open(file_path) as file:
+        string = file.read()
+
+    obj = {}
+    for line in string.splitlines():
+        key, value = line.split(':')
+        obj[key] = value
+
+    return obj
+
+
+RESPONSES_ERROR = "Argument 'acceptableResponses' cannot be of type: '{}'"
 
 
 def getResponse(message, acceptableResponses):
@@ -25,33 +57,7 @@ def getResponse(message, acceptableResponses):
     return response
 
 
-# TODO What about the __file__ attribute
-# TODO Make this automatic if 'numpy' is seen in the source code?
-def getDirsToInclude(string):
-    """
-    Given a string of module names, it will return the 'include' directories
-    essential to their compilation as long as the module has the conventional
-    'get_include' function.
-    """
-    dirs = []
-    a = string.strip()
-    obj = a.split('-')
-
-    if len(obj) == 1 and obj[0]:
-        for module in obj:
-            try:
-                exec('import {}'.format(module))
-            except ImportError:
-                raise FileNotFoundError("The module '{}' does not"
-                                        "exist".format(module))
-            try:
-                dirs.append('-I{}'.format(eval(module).get_include()))
-            except AttributeError:
-                print(NOT_NEEDED_MESSAGE.format(module))
-    return dirs
-
-
-def removeGivensFromTasks(tasks, givens):
+def _removeGivensFromTasks(tasks, givens):
     for given in givens:
         if given in tasks:
             raise Exception("Task '{}' is not supposed to be a"
@@ -62,7 +68,13 @@ def removeGivensFromTasks(tasks, givens):
 
 
 def generateBatches(tasks, givens):
-    removeGivensFromTasks(tasks, givens)
+    """
+    A function to generate a batch of commands to run in a specific order as to
+    meet all the dependencies for each command. For example, the commands with
+    no dependencies are run first, and the commands with the most deep
+    dependencies are run last
+    """
+    _removeGivensFromTasks(tasks, givens)
 
     batches = []
     while tasks:
@@ -72,7 +84,7 @@ def generateBatches(tasks, givens):
                 batch.add(task)
 
         if not batch:
-            batchErrorProcessing(tasks)
+            _batchErrorProcessing(tasks)
 
         for task in batch:
             del tasks[task]
@@ -90,7 +102,7 @@ GIVENS_NOT_SPECIFIED = "The dependencies '{}' should be givens if not " \
                        "specified as tasks"
 
 
-def batchErrorProcessing(tasks):
+def _batchErrorProcessing(tasks):
     should_be_givens = []
     total_deps = {dep for deps in tasks.values() for dep in deps}
     for dep in total_deps:
